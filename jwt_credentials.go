f4675c72694cfffa018a6c1897ca29de2c495718
Travis Cline <travis.cline@gmail.com>
2017-04-14 21:29:51 -0700

    port to use jwt-go

--- jwt_credentials.go
+++ jwt_credentials.go
@@ -6,16 +6,16 @@ import (
 	"os"
 	"regexp"
 	"strings"
-	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
 
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
 
-	"github.com/coreos/go-oidc/jose"
-
 	"golang.org/x/net/context"
 )
 
+// JWTCredentials implements PerRPCCredentials for a jwt in the 'authorization' metadata field.
 type JWTCredentials struct{}
 
 var _ credentials.PerRPCCredentials = &JWTCredentials{}
@@ -25,39 +25,47 @@ var (
 	defaultJWTEnvVar = "GRPCUTILS_JWT_SECRET"
 )
 
-var ErrJWTExpired = errors.New("jwt: expired")
-
 func NewJWTCredentials() *JWTCredentials {
 	return &JWTCredentials{}
 }
 
 var extractBearer = regexp.MustCompile("Bearer (.+)")
 
-func jwtFromAuthorizationBearer(md metadata.MD) (jose.JWT, error) {
+func jwtFromAuthorizationBearer(md metadata.MD, secret string) (*jwt.Token, error) {
 	authn := md["Authorization"]
 	if len(authn) == 0 {
 		authn = md["authorization"]
 	}
 	if len(authn) != 1 {
-		return jose.JWT{}, fmt.Errorf("unexpected number of parts in authorization header(%d)", len(authn))
+		return nil, fmt.Errorf("unexpected number of parts in authorization header(%d)", len(authn))
 	}
 	jwtParts := extractBearer.FindStringSubmatch(authn[0])
 	if len(jwtParts) != 2 {
-		return jose.JWT{}, fmt.Errorf("unexpected number of parts in authorization header(%d)", len(jwtParts))
+		return nil, fmt.Errorf("unexpected number of parts in authorization header(%d)", len(jwtParts))
 	}
-	return jose.ParseJWT(jwtParts[1])
+	return jwt.Parse(jwtParts[1], func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
 }
 
 func (c *JWTCredentials) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
 	md, _ := metadata.FromContext(ctx)
-	jwt, err := jwtFromAuthorizationBearer(md)
+	secret := os.Getenv(defaultJWTEnvVar)
+	if secret == "" {
+		secret = defaultJWTSecret
+	}
+	token, err := jwtFromAuthorizationBearer(md, secret)
 	if err != nil {
 		return nil, err
 	}
-	claims, err := jwt.Claims()
-	if err != nil {
-		return nil, err
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("jwt: invalid token")
 	}
+
 	result := make(map[string]string)
 	for k, v := range claims {
 		val := ""
@@ -73,22 +81,7 @@ func (c *JWTCredentials) GetRequestMetadata(ctx context.Context, uri ...string) 
 		}
 		result[k] = val
 	}
-	secret := os.Getenv(defaultJWTEnvVar)
-	if secret == "" {
-		secret = defaultJWTSecret
-	}
-	verifier, err := jose.NewVerifierHMAC(jose.JWK{
-		Secret: []byte(secret),
-	})
-	if err != nil {
-		return nil, err
-	}
-	if exp, _, err := claims.TimeClaim("exp"); err != nil {
-		return nil, err
-	} else if exp.Before(time.Now()) {
-		return nil, ErrJWTExpired
-	}
-	err = verifier.Verify(jwt.Signature, []byte(jwt.Data()))
+
 	return result, err
 }
 
