4bd14b7cbc8533c5ac240f1fb5723db41807922b
Travis Cline <travis.cline@gmail.com>
2018-06-30 16:55:01 -0700

    protoc-gen-tstypes: demonstrate flags in examples

--- protoc-gen-tstypes/examples.sh
+++ protoc-gen-tstypes/examples.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 set -x
 
 cd testdata
+rm -fr output/*
+mkdir output/defaults output/int-enums output/outpattern output/wo-namespace
 for e in $(ls ./*proto); do
-    protoc -I. --tstypes_out=v=2:output/ "${e}"
+    protoc -I. --tstypes_out=v=1:output/defaults/ "${e}"
+    protoc -I. --tstypes_out=v=1,int_enums=true:output/int-enums/ "${e}"
+    protoc -I. --tstypes_out=v=1,outpattern={{.Dir}}/{{.BaseName}}.d.ts:output/outpattern/ "${e}"
+    protoc -I. --tstypes_out=v=1,declare_namespace=false:output/wo-namespace/ "${e}"
 done
