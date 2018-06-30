1979ab4b6d9dd6ddb0b4606800d65a928a045aee
Travis Cline <travis.cline@gmail.com>
2018-06-30 16:48:02 -0700

    protoc-gen-tstypes: regen examples

--- protoc-gen-tstypes/examples.sh
+++ protoc-gen-tstypes/examples.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 set -x
 
-for e in $(ls ./testdata/example*proto); do
-    protoc -I. --tstypes_out=v=2:testdata/output/ "${e}"
+cd testdata
+for e in $(ls ./*proto); do
+    protoc -I. --tstypes_out=v=2:output/ "${e}"
 done
