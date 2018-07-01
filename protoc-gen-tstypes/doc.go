5aea267de234af23bf592d87575f23d73531ea34
Travis Cline <travis.cline@gmail.com>
2018-07-01 16:44:44 -0700

    protoc-gen-tstypes: doc fixup

--- protoc-gen-tstypes/doc.go
+++ protoc-gen-tstypes/doc.go
@@ -1,13 +1,13 @@
-// Program protoc-gen-tstypes generates TypeScript type definition files from a Protocol Buffer file.
+// Program protoc-gen-tstypes generates TypeScript type declaration files from a Protocol Buffer file.
 //
 // Basic Example:
-// protoc -I. --tstypes_out=. simple.proto
+//  protoc -I. --tstypes_out=. simple.proto
 //
 // See examples.sh for more complex examples (output is in testdata/output)
 //
 // Parameters:
-//  - outpattern: control the output file paths.
-//  - int_enums: use ints instead of strings for enums (default false)
-//  - declare_namespace: declare namespace for the generated type (default true)
-//  - async_iterators: use async iterators for streaming endpoint types (default false)
+//  outpattern: control the output file paths.
+//  int_enums: use ints instead of strings for enums (default false)
+//  declare_namespace: declare namespace for the generated type (default true)
+//  async_iterators: use async iterators for streaming endpoint types (default false)
 package main
