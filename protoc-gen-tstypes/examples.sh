e9b960e3a25db39e84aecfdbb240f4ac86ed7962
Travis Cline <travis.cline@gmail.com>
2020-04-30 06:31:09 -0700

    examples: Include googleapis

--- protoc-gen-tstypes/examples.sh
+++ protoc-gen-tstypes/examples.sh
@@ -7,18 +7,19 @@ rm -fr output/*
 ds=(output/defaults output/int-enums output/camel-case-names output/outpattern-{1,2,3} output/wo-namespace output/async-iterators)
 
 # GOPATH src root relative to the testdata directory
-gopath_root="../../../../../"
+GOPATH_ROOT="../../../../../"
+GOOGLEAPIS_ROOT="${GOPATH_ROOT}/github.com/googleapis/googleapis"
 
 mkdir -p ${ds[*]}
-for e in $(ls ./*proto); do
-    protoc -I. -I${gopath_root} --tstypes_out=v=1:output/defaults/ "${e}"
-    protoc -I. -I${gopath_root} --tstypes_out=v=1,int_enums=true:output/int-enums/ "${e}"
-    protoc -I. -I${gopath_root} --tstypes_out=v=1,original_names=false:output/camel-case-names/ "${e}"
-    protoc -I. -I${gopath_root} --tstypes_out=v=1,outpattern={{.Dir}}/{{.BaseName}}.d.ts:output/outpattern-1/ "${e}"
-    protoc -I. -I${gopath_root} --tstypes_out 'v=1,outpattern={{.Descriptor.GetPackage | replace "." "/"}}/{{.BaseName}}.d.ts:output/outpattern-2/' "${e}"
-    protoc -I. -I${gopath_root} --tstypes_out 'v=1,outpattern={{.Dir}}/{{.BaseName}}pb.d.ts:output/outpattern-3/' "${e}"
-    protoc -I. -I${gopath_root} --tstypes_out=v=1,declare_namespace=false:output/wo-namespace/ "${e}"
-    protoc -I. -I${gopath_root} --tstypes_out=v=1,async_iterators=true:output/async-iterators/ "${e}"
+for e in ./*proto; do
+    protoc -I. -I${GOPATH_ROOT} -I${GOOGLEAPIS_ROOT} --tstypes_out=v=1:output/defaults/ "${e}"
+    protoc -I. -I${GOPATH_ROOT} -I${GOOGLEAPIS_ROOT} --tstypes_out=v=1,int_enums=true:output/int-enums/ "${e}"
+    protoc -I. -I${GOPATH_ROOT} -I${GOOGLEAPIS_ROOT} --tstypes_out=v=1,original_names=false:output/camel-case-names/ "${e}"
+    protoc -I. -I${GOPATH_ROOT} -I${GOOGLEAPIS_ROOT} --tstypes_out=v=1,outpattern={{.Dir}}/{{.BaseName}}.d.ts:output/outpattern-1/ "${e}"
+    protoc -I. -I${GOPATH_ROOT} -I${GOOGLEAPIS_ROOT} --tstypes_out 'v=1,outpattern={{.Descriptor.GetPackage | replace "." "/"}}/{{.BaseName}}.d.ts:output/outpattern-2/' "${e}"
+    protoc -I. -I${GOPATH_ROOT} -I${GOOGLEAPIS_ROOT} --tstypes_out 'v=1,outpattern={{.Dir}}/{{.BaseName}}pb.d.ts:output/outpattern-3/' "${e}"
+    protoc -I. -I${GOPATH_ROOT} -I${GOOGLEAPIS_ROOT} --tstypes_out=v=1,declare_namespace=false:output/wo-namespace/ "${e}"
+    protoc -I. -I${GOPATH_ROOT} -I${GOOGLEAPIS_ROOT} --tstypes_out=v=1,async_iterators=true:output/async-iterators/ "${e}"
 done
 
 if [ "${CHECK:-}" != "0" ]; then
